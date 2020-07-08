0c235a872fe9528594a6018382c007ed48a119da
Philip Pika <pp15841@bristol.ac.uk>
2020-07-08 16:08:48 +0200

    Added mocsy_module.py with new path conditions

---
+++ A_source_code/carbon/code/mocsy_module.py
@@ -0,0 +1,24 @@
+# ******************************************************
+## Copyright 2019, PBL Netherlands Environmental Assessment Agency and Utrecht University.
+## Reuse permitted under Gnu Public License, GPL v3.
+# ******************************************************
+'''
+imports mocsy module
+'''
+try: 
+  import mocsy
+except ModuleNotFoundError:
+  import os
+  import sys
+  root = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..')
+  if "/User" in root:
+      # working in Mac OS
+      path = os.path.join(root,"libs","mocsy","mac")
+  elif "/work" in root:
+      # working in linux OS
+      path = os.path.join(root,"libs","mocsy","linux")
+  elif "/" in root:
+      # working in linux OS - original statement
+      path = os.path.join(root,"libs","mocsy","linux")
+  if (os.path.exists(path)):
+      sys.path.insert(3, path)
