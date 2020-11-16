b1e6e0a26189f1a925b5c84a432047199eaaf7a3
Philip Pika <pp15841@bristol.ac.uk>
2020-11-16 09:24:39 +0100

    Changed the simulation period

--- Test.sh
+++ Test.sh
@@ -22,8 +22,8 @@ rm -r $DIR/OUT
 
 echo "Mess: Start SpinUp"
 #python dgnm_main.py --endtime=1959 --maskid=800 --lspinup=1 --inifile ../ini/cmd_y_SS_bio.ini
-python dgnm_main.py --endtime=1952 --maskid=800 --lspinup=1 --inifile ../ini/cmd_m_50yrs_bio_def.ini
-cp $DIR/OUT/bio/pkl/start1951.000.pkl $DIR/A_source_code/carbon/startups/start1951.000.pkl
+python dgnm_main.py --starttime=1990 --endtime=1992 --maskid=800 --lspinup=1 --inifile ../ini/cmd_m_50yrs_bio_def.ini
+cp $DIR/OUT/bio/pkl/start1991.000.pkl $DIR/A_source_code/carbon/startups/start1991.000.pkl
 echo "Note: First iteration done"
 
 #python dgnm_main.py --endtime=1952 --maskid=800 --lspinup=0 --inifile ../ini/Testing_cmd_m_50yrs_bio_def.ini
