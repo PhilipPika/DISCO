0c55a6df68e62c00c71f7487e13d210f89424aa2
Philip Pika <pp15841@bristol.ac.uk>
2021-01-11 18:14:22 +0100

    Minor changes

--- perturbation.sh
+++ perturbation.sh
@@ -5,7 +5,7 @@ eval "$(/usr/local/anaconda3/bin/conda shell.zsh hook)";conda activate p37
 # DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo "Mess: Exporting Dir for model"
 
-DIR=/Users/pippo/Documents/SurfDrive/Research/Projects/UU_project/MyVersion/DISCO
+DIR=/Users/pippo/Documents/SurfDrive/Research/Projects/DISCO_project/MyVersion/DISCO
 TOCODE=$DIR/A_source_code
 
 export DGNM_ROOT=$TOCODE
@@ -21,14 +21,14 @@ echo "Mess: Removing current results"
 rm -r $DIR/OUT
 
 Spinstart=2000
-Spinend=2002
-SimuEnd=2010 #Remember to also change the file name in params.ini
+Spinend=2001
+SimuEnd=2002 #Remember to also change the file name in params.ini
 echo "Mess: Start SpinUp"
-nice -n 19 python dgnm_main.py --starttime=$Spinstart --endtime=$Spinend --maskid=13 --lspinup=1 --inifile ../ini/cmd_m_50yrs_bio_def.ini --parameter_ini ../ini/params_m_Q_and_T.ini
+nice -n 19 python dgnm_main.py --starttime=$Spinstart --endtime=$Spinend --maskid=99 --lspinup=1 --inifile ../ini/cmd_m_50yrs_bio_def.ini --parameter_ini ../ini/params_m_Q_and_T.ini
 cp $DIR/OUT/bio/pkl/start$Spinend.000.pkl $DIR/A_source_code/carbon/startups/start$Spinstart.000.pkl
 echo "Note: First iteration done"
 
-nice -n 19 python dgnm_main.py  --starttime=$Spinstart --endtime=$SimuEnd --maskid=13 --lspinup=0 --inifile ../ini/cmd_m_50yrs_bio_def.ini --parameter_ini ../ini/params_m_Q_and_T.ini
+nice -n 19 python dgnm_main.py  --starttime=$Spinstart --endtime=$SimuEnd --maskid=99 --lspinup=0 --inifile ../ini/cmd_m_50yrs_bio_def.ini --parameter_ini ../ini/params_m_Q_and_T.ini
 cp $DIR/OUT/bio/pkl/start$SimuEnd.000.pkl $DIR/A_source_code/carbon/startups/start$Spinstart.000.pkl
 
 #
